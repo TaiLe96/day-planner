b8caba705b890ffa35a0045a24c3e35409ea0bf0
Tai Le <tle449@gmail.com>
2019-12-08 20:55:46 -0800

    index.html style.css script.js

---
+++ asset/script.js
@@ -0,0 +1,6 @@
+//------------------Current Date--------------------------
+let currentDate = document.getElementById("current-date");
+let todayDate = Date(19, 12, 9);
+currentDate.innerHTML = todayDate;
+
+//------------------Save to local storage-----------------
